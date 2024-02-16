941bed45dbf49dc45c6b953767313a783734858f
JoaoGuimaraesTDS2023 <joao.guimaraes.tds2023@gmail.com>
2024-02-16 15:33:36 -0300

    atualizando exercicios

--- aula2/exercicio2.php
+++ aula2/exercicio2.php
@@ -6,7 +6,7 @@
     do
     {
         $total += $valor;
-        $valor = readline("Valor: R$");
+        $valor = (int) readline("Valor: R$");
 
     } while ($valor != 0);
 
--- aula2/exercicio6.php
+++ aula2/exercicio6.php
@@ -1,15 +1,32 @@
 <?php
 
+    $num = array();
+
     echo "Multiplicação de inteiros: \n";
 
-    $num1 = (int) readline("Primeiro Número: ");
-    $num2 = (int) readline("Segundo Número: ");
-    $resultado = $num1;
+    $num[0] = (int) readline("Primeiro Número: ");
+    $num[1] = (int) readline("Segundo Número: ");
 
-    for ($i = 1; $i < $num2; $i++) { 
-        $resultado += $num1;
+    $menor = min($num);
+    $maior = max($num);
+    $maior = ($maior < 0) ? $maior-$maior-$maior : $maior;
+
+    $resultado = $menor;
+
+    if ($maior == 0 or $menor == 0) {
+        echo "RESULTADO: 0 \n";
+
+    }else{
+        
+        for ($i = 1; $i < $maior; $i++) { 
+            $resultado += $menor;
+        }
+
+        $resultado = (max($num) < 0 and $menor < 0) ? $resultado-$resultado-$resultado : $resultado;
+
+        echo "RESULTADO: $resultado \n" ;
     }
 
-    echo "RESULTADO: $resultado \n";
+    
 
 ?>
