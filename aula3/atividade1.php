6f6ed189d04222d8770744f5eb62cb0b91ef0fba
JoaoGuimaraesTDS2023 <joao.guimaraes.tds2023@gmail.com>
2024-02-23 13:53:49 -0300

    adicionando atividade1

---
+++ aula3/atividade1.php
@@ -0,0 +1,30 @@
+<?php
+
+    $numero = 10;
+    echo "Para sair digite um número > 2. \n";
+
+    while ($numero > 2){
+
+        $numero = (int) readline("Digite um número: ");
+
+        echo (verificaPrimo($numero)) ? "$numero é primo" : "";
+
+        echo "\n";
+
+    } ;
+
+    function verificaPrimo($num): bool
+    {
+        $divisores = 0;
+
+        for ($i=2; $i<$num ; $i++)
+        { 
+            $divisores = ($num % $i != 0) ? $divisores : $divisores+1;
+        }
+
+        $primo = ($divisores > 0) ? false : true;
+
+        return $primo; 
+    }
+
+?>
